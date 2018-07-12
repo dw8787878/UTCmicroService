290efbfede6ea1283d52d7d80f5b142eda367387
dw8787878 <darryn8wu@gmail.com>
2018-07-12 08:41:29 -0400

    fixing express routes

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Hey there D!') )
+//app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.sendFile('./public'));
+
+app.post('/', (req, res) => res.send('We got your post!') )
 
 app.listen(port, (err)=>{
   if (err) {
