fda77bbd80661b7a3777cd7076fb79ced6880bc2
dw8787878 <darryn8wu@gmail.com>
2018-07-16 12:16:15 -0400

    resolved path to absolute

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {resolve} = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -9,7 +10,10 @@ app.use(bodyParser.json())
 
 //app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.sendFile('./public'));
+//app.get('/', (req, res) => res.sendFile('./public'));
+
+app.get('/', (req, res) => res.sendFile(resolve(__dirname, '..', '/Users/darrynwu/UTCmicroService/public', 'index.html')));
+//.get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html')))
 
 app.post('/', (req, res) => res.send('We got your post!') )
 
