5828ca1a0078d7701cbc5b6a7bec26c0191ce8b5
dw8787878 <darryn8wu@gmail.com>
2018-07-16 12:17:32 -0400

    same as before

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
 
-//app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public')));
-
-//app.get('/', (req, res) => res.sendFile('./public'));
-
 app.get('/', (req, res) => res.sendFile(resolve(__dirname, '..', '/Users/darrynwu/UTCmicroService/public', 'index.html')));
-//.get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html')))
 
 app.post('/', (req, res) => res.send('We got your post!') )
 
