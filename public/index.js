870e439e5d35980b3d4976af4bd976e17283f4d4
dw8787878 <darryn8wu@gmail.com>
2018-08-03 10:25:07 -0400

    bodyparsing post dateString, and adding validator

--- public/index.js
+++ public/index.js
@@ -1,18 +1,18 @@
 function submitform() {
-  let newName = 'John Smith',
+  //let newName = 'John Smith',
       xhr = new XMLHttpRequest();
-
+  //console.log('here1')
   xhr.open('POST', '/');
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      alert('Success!');
-    }
-    else if (xhr.status !== 200) {
-      alert('Request failed.  Returned status of ' + xhr.status);
-    }
+  //console.log('here')
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded..blah');
+  // xhr.onload = function () {
+  //   if (xhr.status === 200) {
+  //     alert('Success!');
+  //   }
+  //   else if (xhr.status !== 200) {
+  //     alert('Request failed.  Returned status of ' + xhr.status);
+  //   }
 
-    xhr.send(encodeURI('name=' + newName));''
+   // xhr.send(encodeURI('name=' + newName));
 
   }
-}
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const {resolve} = require('path');
 const bodyParser = require('body-parser');
+const {body, validationResult} = require('express-validator/check');
+const {sanitizeBody} = require('express-validator/filter');
 const app = express();
 
 const port = 3000;
@@ -10,11 +12,15 @@ app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.sendFile(resolve(__dirname, '..', '/Users/darrynwu/UTCmicroService/public', 'index.html')));
 
-app.post('/', (req, res) => res.send('We got your post!') )
+app.post('/', (req, res) => {
+  let dateString = req.body.date_string;
+  //console.log('this is the req.body:', req.body);
+  res.send(`We got your post! ${dateString}`)
+});
 
 app.listen(port, (err)=>{
   if (err) {
-    return console.log('something bad happened', err);
+    return console.log('wtf happened?', err);
   }
   console.log(`Server is running local at port: ${port}`);
 });
