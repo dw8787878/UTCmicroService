d3607d3786faad26b42258e1777cfaadc1f15ed1
dw8787878 <darryn8wu@gmail.com>
2018-08-05 10:31:37 -0400

    modified sanitize

--- public/index.js
+++ public/index.js
@@ -1,18 +1,18 @@
-function submitform() {
-  //let newName = 'John Smith',
-      xhr = new XMLHttpRequest();
-  //console.log('here1')
-  xhr.open('POST', '/');
-  //console.log('here')
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded..blah');
-  // xhr.onload = function () {
-  //   if (xhr.status === 200) {
-  //     alert('Success!');
-  //   }
-  //   else if (xhr.status !== 200) {
-  //     alert('Request failed.  Returned status of ' + xhr.status);
-  //   }
+// function submitform() {
+//   //let newName = 'John Smith',
+//       xhr = new XMLHttpRequest();
+//   //console.log('here1')
+//   xhr.open('POST', '/');
+//   //console.log('here')
+//   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded..blah');
+//   // xhr.onload = function () {
+//   //   if (xhr.status === 200) {
+//   //     alert('Success!');
+//   //   }
+//   //   else if (xhr.status !== 200) {
+//   //     alert('Request failed.  Returned status of ' + xhr.status);
+//   //   }
 
-   // xhr.send(encodeURI('name=' + newName));
+//    // xhr.send(encodeURI('name=' + newName));
 
-  }
+//   }
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const {resolve} = require('path');
 const bodyParser = require('body-parser');
 const {body, validationResult} = require('express-validator/check');
-const {sanitizeBody} = require('express-validator/filter');
 const app = express();
+
+const sanitize = require('./sanitize');
+const {convertUTC} = require('./convertUTC');
 
 const port = 3000;
 
@@ -14,16 +16,17 @@ app.get('/', (req, res) => res.sendFile(resolve(__dirname, '..', '/Users/darrynw
 
 app.post('/', (req, res) => {
   let dateString = req.body.date_string;
-  //console.log('this is the req.body:', req.body);
+
+  //Sanitizes input from front end
+  let validFlag = sanitize.verify(dateString);
 
   let testValArr = dateString.split('/');
-  let itemStr, itemDate, validFlag;
-  let re = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
-
+  let itemStr, itemDate;
 
 
   if (validFlag){
-    res.send(`We got your post! ${dateString}`)
+
+    res.send(`This is your input: ${dateString} <br> This is the converted UTC format: ${convertUTC()}`)
   } else {
     res.send("Your input is not a valid ISO 8601 date string.  Please see: <a href='https://en.wikipedia.org/wiki/ISO_8601'>wiki ISO 8601</a>");
   }
